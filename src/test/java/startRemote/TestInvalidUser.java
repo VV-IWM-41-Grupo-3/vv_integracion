cf5cc631c803d0f3ea99b3c3130d67b97a012f39
IkerMoratinoRodriguez <iker.moratino.rodriguez@alumnos.upm.es>
2021-12-06 11:58:25 +0100

    Tests de start and stop updated

--- src/test/java/startRemote/TestInvalidUser.java
+++ src/test/java/startRemote/TestInvalidUser.java
@@ -4,6 +4,8 @@ import com.practica.integracion.DAO.AuthDAO;
 import com.practica.integracion.DAO.GenericDAO;
 import com.practica.integracion.DAO.User;
 import com.practica.integracion.manager.SystemManager;
+import com.practica.integracion.manager.SystemManagerException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
@@ -11,14 +13,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.naming.OperationNotSupportedException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TestInvalidUser {
@@ -31,42 +33,45 @@ public class TestInvalidUser {
     @Test
     public void testStartRemoteSystemWithInvalidUserValidSystem() throws Exception {
 
-        User invalidUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
-        Mockito.lenient().when(mockAuthDao.getAuthData(invalidUser.getId())).thenReturn(null);
+        User invalidUser = null;
+        //User invalidUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+        String userId = "1";
+        lenient().when(mockAuthDao.getAuthData(userId)).thenReturn(null);
 
         String validId = "12345"; // id valido de sistema
-        ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
-        Mockito.lenient().when(mockGenericDao.getSomeData(invalidUser, "where id=" + validId)).thenReturn(lista);
+        lenient().when(mockGenericDao.getSomeData(invalidUser, "where id=" + validId)).thenReturn(null);
         // primero debe ejecutarse la llamada al dao de autenticación
         // despues el de  acceso a datos del sistema (la validaciones del orden en cada prueba)
         InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
         // instanciamos el manager con los mock creados
         SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
         // llamada al api a probar
-        Collection<Object> retorno = manager.startRemoteSystem(invalidUser.getId(), validId);
-        assertEquals(retorno.toString(), "[]");
+        Collection<Object> retorno = manager.startRemoteSystem(userId, validId);
+        assertNull(retorno);
+        //Poner en la memoria que se asume que se devuelve null en el caso de que el usuario sea inválido y el sistema válido ya que en la documentación no se especifica qué ocurre en este caso
         // vemos si se ejecutan las llamadas a los dao, y en el orden correcto
-        ordered.verify(mockAuthDao).getAuthData(invalidUser.getId());
+        ordered.verify(mockAuthDao).getAuthData(userId);
         ordered.verify(mockGenericDao).getSomeData(invalidUser, "where id=" + validId);
     }
 
     @Test
     public void testStartRemoteSystemWithInvalidUserAndSystem() throws Exception {
-        User inValidUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
-        when(mockAuthDao.getAuthData(inValidUser.getId())).thenReturn(null);
+        User invalidUser = null;
+        String userId = "1";
+        lenient().when(mockAuthDao.getAuthData(userId)).thenReturn(null);
 
         String invalidId = "12345";
         ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
-        Mockito.lenient().when(mockGenericDao.getSomeData(inValidUser, "where id=" + invalidId)).thenReturn(null);
+        when(mockGenericDao.getSomeData(invalidUser, "where id=" + invalidId)).thenReturn(null);
 
         InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
 
         SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
 
-        Collection<Object> retorno = manager.startRemoteSystem(inValidUser.getId(), invalidId);
+        Collection<Object> retorno = manager.startRemoteSystem(userId, invalidId);
         assertNull(retorno);
         //Poner en la memoria que se asume que se devuelve null en el caso de que el usuario sea válido y el sistema inválido ya que en la documentación no se especifica qué ocurre en este caso
-        ordered.verify(mockAuthDao).getAuthData(inValidUser.getId());
-        ordered.verify(mockGenericDao).getSomeData(inValidUser, "where id=" + invalidId);
+        ordered.verify(mockAuthDao).getAuthData(userId);
+        ordered.verify(mockGenericDao).getSomeData(invalidUser, "where id=" + invalidId);
     }
 }
