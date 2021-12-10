264f49e9ed578d9db02e8212961524d30e39a99a
ignacio <ignacio.gomez.oliva@alumnos.upm.es>
2021-12-10 20:26:29 +0100

    Revisión de los métodos de usuario inválido de deleteRemoteSystem.

--- src/test/java/com/practica/integracion/TestDeleteRemote/TestInvalidUser.java
+++ src/test/java/com/practica/integracion/TestDeleteRemote/TestInvalidUser.java
@@ -1,5 +1,6 @@
 package com.practica.integracion.TestDeleteRemote;
 
+import com.practica.integracion.DAO.AuthDAO;
 import com.practica.integracion.DAO.GenericDAO;
 import com.practica.integracion.DAO.User;
 import com.practica.integracion.manager.SystemManager;
@@ -7,41 +8,58 @@ import com.practica.integracion.manager.SystemManagerException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.naming.OperationNotSupportedException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TestInvalidUser {
 	@Mock
-	GenericDAO dao;
+	GenericDAO mockGenericDao;
+	@Mock
+	AuthDAO mockAuthDao;
 
 	@Test
 	public void testDeleteUserInvalidGood() throws OperationNotSupportedException {
-		final User auth = null;
-		lenient().when(dao.deleteSomeData(auth, "123")).thenReturn(false);
-
-		SystemManager sysm = new SystemManager(null, dao);
+		User invalidUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		//lenient().when(mockAuthDao.getAuthData(invalidUser.getId())).thenReturn(null);
+		//Por la implementación del método, esto no se utiliza ya que no hace la llamada
+		//Al metodo de verificacion
+		String validId = "12345";
+		lenient().when(mockGenericDao.deleteSomeData(null, validId)).thenThrow(OperationNotSupportedException.class);
+		InOrder ordered = inOrder(mockGenericDao);
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
 		Assertions.assertThrows(SystemManagerException.class, () ->{
-			sysm.deleteRemoteSystem("1", "123");
+			manager.deleteRemoteSystem(invalidUser.getId(), validId);
 		});
-		verify(dao, times(1)).deleteSomeData(auth, "123");
+		//ordered.verify(mockAuthDao).getAuthData(invalidUser.getId());
+		//Lo mismo pasa con esta linea
+		ordered.verify(mockGenericDao).deleteSomeData(null, validId);
 	}
-
 	@Test
 	public void testDeleteUserInvalidBad() throws OperationNotSupportedException {
-		final User auth = null;
-		lenient().when(dao.deleteSomeData(auth, "123")).thenReturn(false);
-
-		SystemManager sysm = null;
+		User invalidUser = new User("1","Ana","Lopez","Madrid", new ArrayList<Object>(Arrays.asList(1, 2)));
+		//lenient().when(mockAuthDao.getAuthData(invalidUser.getId())).thenReturn(null);
+		//Por la implementación del método, esto no se utiliza ya que no hace la llamada
+		//Al metodo de verificacion
+		String validId = "12345";
+		lenient().when(mockGenericDao.deleteSomeData(null, validId)).thenThrow(OperationNotSupportedException.class);
+		InOrder ordered = inOrder(mockGenericDao);
+		SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
 		Assertions.assertThrows(SystemManagerException.class, () ->{
-			sysm.deleteRemoteSystem("1", "123");
+			manager.deleteRemoteSystem(invalidUser.getId(), validId);
 		});
-		verify(dao, times(1)).deleteSomeData(auth, "123");
+		//ordered.verify(mockAuthDao).getAuthData(invalidUser.getId());
+		//Lo mismo pasa con esta linea
+		ordered.verify(mockGenericDao).deleteSomeData(null, validId);
 	}
+
 }
